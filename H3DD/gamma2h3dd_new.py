13173471870974632bc0ce39b711ef0834e1d7c2
Patrick Yang <patrick.yang880612@gmail.com>
2024-08-07 10:11:22 +0800

    add some note.

--- H3DD/gamma2h3dd_new.py
+++ H3DD/gamma2h3dd_new.py
@@ -14,34 +14,6 @@ def convert_utc_datetime(utc_datetime_str):
     """
     return datetime.strptime(utc_datetime_str, '%Y-%m-%dT%H:%M:%S.%f')
 
-def process_file(file):
-    event_window_id = file.stem
-    with open(file, 'r') as r:
-        reader = csv.reader(r)
-        header = next(reader)
-        data = []
-        for line in reader:
-            line.insert(0, event_window_id)
-            data.append(line)
-    data.sort(key=lambda row: row[-1])
-    return header, data
-def concat_picks(gamma_picks_dir, all_picks):
-    files = list(gamma_picks_dir.rglob('*csv'))
-    headers_written = False
-
-    with ThreadPoolExecutor(max_workers=40) as executor:
-        results = list(executor.map(process_file, files)) # using map will indeed wait for all threads
-
-    with open(all_picks, 'w', newline='') as f:
-        writer = csv.writer(f)
-        
-        for header, data in results:
-            if not headers_written:
-                writer.writerow(header)
-                headers_written = True
-            writer.writerows(data)
-    return all_picks
-
 def gamma_reorder(ori_csv, reorder_csv):
     
     with open(ori_csv, 'r') as file:
@@ -87,19 +59,12 @@ def transform(index, split_dir, gamma_picks, output_dir):
             event_index = row['event_index']
             logging.info(f"***Event {event_index}***")
             r.write(f"{row['ymd']:>9}{row['hour']:>2}{row['minute']:>2}{row['seconds']:>6.2f}{row['lat_int']:2}{row['lat_deg']:0>5.2f}{row['lon_int']:3}{row['lon_deg']:0>5.2f}{row['depth']:>6.2f}\n")
-            check_box = []
             for _, pick_row in df_picks[df_picks['event_index'] == event_index].iterrows():
                 if row['minute'] == 59 and pick_row['minute'] == 0: 
                     wmm = int(60)
                 else:
                     wmm = pick_row['minute']
                 weight = '1.00'
-                check_pattern = f"{pick_row['phase_type']}_{pick_row['minute']}_{pick_row['seconds']}"
-                if check_pattern not in check_box:
-                    check_box.append(check_pattern)
-                else:
-                    logging.info(f"{pick_row['station_id']} has same {pick_row['phase_type']} arrival time")
-                    continue
                 if pick_row['phase_type'] == 'P':
                     r.write(f"{' ':1}{pick_row['station_id']:<4}{'0.0':>6}{'0':>4}{'0':>4}{wmm:>4}{pick_row['seconds']:>6.2f}{'0.01':>5}{weight:>5}{'0.00':>6}{'0.00':>5}{'0.00':>5}\n")
                 else:
@@ -108,12 +73,11 @@ def transform(index, split_dir, gamma_picks, output_dir):
     logging.info(f'gamma_event_{index} transform is done')                    
 
 if __name__ == '__main__':
-    # ori_csv = Path('/home/patrick/Work/EQNet/tests/hualien_0403/result/gamma_events.csv')
-    reorder_csv = Path('/home/patrick/Work/EQNet/tests/hualien_0403/gamma_seis_das/gamma_order.csv')
-    split_dir = Path('/home/patrick/Work/EQNet/tests/hualien_0403/gamma_seis_das/split_dir')
-    # gamma_picks_dir = Path('/home/patrick/Work/EQNet/tests/hualien_0403/output/')
-    gamma_picks = Path('/home/patrick/Work/EQNet/tests/hualien_0403/gamma_seis_das/gamma_picks.csv')
-    output_dir = Path('/home/patrick/Work/EQNet/tests/hualien_0403/gamma_seis_das/for_h3dd')
+    ori_csv = Path('/home/patrick/Work/EQNet/tests/hualien_0403/result/gamma_events.csv') # gamma_events.csv
+    reorder_csv = Path('/home/patrick/Work/EQNet/tests/hualien_0403/gamma_seis_das/gamma_order.csv') # the path of reorder csv you want to name
+    split_dir = Path('/home/patrick/Work/EQNet/tests/hualien_0403/gamma_seis_das/split_dir') # the path put the split files
+    gamma_picks = Path('/home/patrick/Work/EQNet/tests/hualien_0403/gamma_seis_das/gamma_picks.csv') # gamma_picks.csv
+    output_dir = Path('/home/patrick/Work/EQNet/tests/hualien_0403/gamma_seis_das/for_h3dd') # where the output format will generate.
 
     logging.basicConfig(
         filename='2h3dd.log',
@@ -123,15 +87,11 @@ if __name__ == '__main__':
         filemode='w'
         )
     
-    # gamma_reorder(ori_csv, reorder_csv)
-    # gamma_chunk_split(split_dir, reorder_csv)
-    
-    # check = True
-    # if not check:
-    #     gamma_picks = concat_picks(gamma_picks_dir, gamma_picks)
+    gamma_reorder(ori_csv, reorder_csv) # makes gamma event follow the oreder
+    gamma_chunk_split(split_dir, reorder_csv) # split the gamma event due to the limitation of h3dd in a execution is 5000 events. (we set 4000) 
 
-    chunk_num = len(os.listdir(split_dir))
-    index_list = np.arange(0, chunk_num)
+    chunk_num = len(os.listdir(split_dir)) # count the num of split files.
+    index_list = np.arange(0, chunk_num) # create a list that was utilized to ietrate.
     print("let's go")
     with ThreadPoolExecutor(max_workers=chunk_num) as executor:
         executor.map(transform, index_list, [split_dir] * chunk_num, [gamma_picks] * chunk_num, [output_dir] * chunk_num)
